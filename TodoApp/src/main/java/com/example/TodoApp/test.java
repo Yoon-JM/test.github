e90f3441e70997e454675b84b2276661849ebc28
Yoon-JM <wlals3591@gmail.com>
2023-04-12 17:29:18 +0900

    initial commit

--- TodoApp/src/main/java/com/example/TodoApp/test.java
+++ TodoApp/src/main/java/com/example/TodoApp/test.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class test {
 
-    String test = "To-do Application";
+    String test = "To-do Application!!";
 
     @GetMapping("/")
     public String helloWorld(){
